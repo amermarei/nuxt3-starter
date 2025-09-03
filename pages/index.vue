ee4ad749f06b0093a2ecf9856d992839a6e01453
Amir marei <amermarei444@gmail.com>
2025-09-03 22:00:10 +0300

    feat: setup icons

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div class="min-h-screen flex flex-col gap-4 items-center justify-center bg-blue-600">
     <h1 class="text-5xl font-bold text-white underline">Tailwind is working</h1>
     <p class="text-4xl font-bold text-white">{{ t("hello") }}</p>
+    <Icon name="ic:sharp-facebook" size="24" />
   </div>
 </template>
 <script setup lang="ts">
